ec63e975afd5c63a116e7a7145904bdbfffc3b3d
Jimmy Cleveland <swashbucklingwithcode@gmail.com>
2020-09-13 17:26:15 -0600

    basic image adding

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,15 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        name: 'data',
         path: `${__dirname}/src/data`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`
       }
     },
     'gatsby-transformer-remark',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+// import treeInForest from '../images/veeterzy-tree-in-forest.jpg'
 
-const Index = () => {
+const Index = ({ data }) => {
   return (
     <div>
       <h1>The homepage</h1>
+      <img src={data.file.publicURL} alt="Tree in Forest" style={{ maxWidth: 800 }} />
+      <p>
+        <span>
+          Photo by{' '}
+          <a href="https://unsplash.com/@veeterzy?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+            veeterzy
+          </a>{' '}
+          on{' '}
+          <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </span>
+      </p>
     </div>
   )
 }
+
+export const homepageQuery = graphql`
+  query HomepageQuery {
+    file(relativePath: {eq: "veeterzy-tree-in-forest.jpg"}) {
+      publicURL
+    }
+  }
+`
 
 export default Index
