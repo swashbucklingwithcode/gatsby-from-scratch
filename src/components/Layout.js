2eeccdb86cb99f366d27f9331f7b155e0e9df9be
Jimmy Cleveland <swashbucklingwithcode@gmail.com>
2020-10-27 09:27:16 -0600

    emotion

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
 const Layout = ({ children }) => {
   return (
@@ -11,12 +12,36 @@ const Layout = ({ children }) => {
   )
 }
 
-const Footer = () => {
+const Footer = ({ light }) => {
   return (
-    <footer className="footer">
+    <StyledFooter light={light}>
       &copy; Copyright 2020
-    </footer>
+      <ul className="links-column">
+        <li><a href="">First</a></li>
+        <li><a href="">Second</a></li>
+        <li><a href="">Third</a></li>
+        <li><a href="">Fourth</a></li>
+        <li><a href="">Fifth</a></li>
+      </ul>
+    </StyledFooter>
   )
 }
+
+const StyledFooter = styled.footer`
+  margin-top: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100px;
+  background: #372e28;
+  color: #f3eee4;
+  background-color: ${(props) => props.light ? 'oldlace' : '#372e28'};
+  color: ${(props) => props.light ? '#372e28' : '#f3eee4'};
+
+  a {
+    color: red;
+    text-decoration: none;
+  }
+`
 
 export default Layout
