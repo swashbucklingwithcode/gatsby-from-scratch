00c0371fcebf68eb7f364d3b8fc9be650caf3a52
Jimmy Cleveland <swashbucklingwithcode@gmail.com>
2020-11-02 16:48:06 -0700

    add plugin manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,5 +35,23 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Cheese Extravaganza",
+        short_name: "CheeseEx",
+        start_url: "/",
+        background_color: "#5b3e29",
+        theme_color: "#fdf5e6",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
 }
