202702ead2b866b80a985483d190cc67945d339e
Jimmy Cleveland <swashbucklingwithcode@gmail.com>
2020-12-04 11:54:38 -0700

    Style homepage posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
 import Helmet from 'react-helmet'
+import styled from '@emotion/styled'
 
 const Index = ({ data }) => {
   return (
@@ -15,16 +16,49 @@ const Index = ({ data }) => {
       />
       <h1>The homepage</h1>
       {data.allMarkdownRemark.edges.map(({ node: post }) => (
-        <Link to={post.fields.slug}>
-          <h3>{post.frontmatter.title}</h3>
-          <p>{post.frontmatter.date}</p>
-          <p>{post.excerpt}</p>
-          <p>A lovely {post.timeToRead} minute read.</p>
-        </Link>
+        <PostCard to={post.fields.slug}>
+          <h3 className="title">{post.frontmatter.title}</h3>
+          <p className="ttr">{post.timeToRead} minute read</p>
+          <p className="excerpt">{post.excerpt}</p>
+          <p className="date">{post.frontmatter.date}</p>
+        </PostCard>
       ))}
     </Layout>
   )
 }
+
+const PostCard = styled(Link)`
+  display: block;
+  margin: 0 0 2rem;
+  color: var(--text);
+  text-decoration: none;
+  background: #f3eee4;
+  padding: 1rem 1.5rem;
+  border-radius: 5px;
+
+  .title {
+    margin: 0 0 0.3rem;
+    font-size: 20px;
+  }
+  .ttr {
+    margin: 0 0 0.7rem;
+    font-size: 14px;
+    color: #ae4900;
+  }
+  .excerpt {
+    margin: 0 0 0.5rem;
+    font-size: 16px;
+  }
+  .date {
+    display: inline-block;
+    margin: 0;
+    background-color: #726c68;
+    color: oldlace;
+    border-radius: 999px;
+    padding: 3px 10px;
+    font-size: 12px;
+  }
+`
 
 export const homepageQuery = graphql`
   query HomepageQuery {
